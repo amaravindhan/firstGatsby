48d31eabfaa14ac90db49ab9ee15462d2a82e7ef
Aravindhan Sundaram <aravindhansundram13@gmail.com>
2021-08-09 21:14:37 +0530

    added new blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Learning Site",
+    title: "Learning Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
   ],
 };
